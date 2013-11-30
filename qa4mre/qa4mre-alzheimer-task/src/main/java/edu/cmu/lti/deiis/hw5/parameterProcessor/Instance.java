e4e11147255db702dd107128d7068d84eee349d1
Mengda Yang <mengda.yang@gmail.com>
2013-11-30 02:16:31 -0500

    Fixed bugs.

--- qa4mre/qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/parameterProcessor/Instance.java
+++ qa4mre/qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/parameterProcessor/Instance.java
@@ -3,14 +3,36 @@ package edu.cmu.lti.deiis.hw5.parameterProcessor;
 import java.util.ArrayList;
 
 public class Instance {
-  public Double y;
-  public ArrayList<Double> x;
-  public Instance(String input){
-    String[] numbers = input.split(" ");
-    y = Double.parseDouble(numbers[0]);
-    x = new ArrayList<Double>();
-    for(int i = 1; i < numbers.length; ++i){
-      x.add(Double.parseDouble(numbers[i]));
-    }
-  }
+	public Double y;
+	public ArrayList<Double> x;
+
+	public Instance(String input) {
+		String[] numbers = input.split(" ");
+		y = Double.parseDouble(numbers[0]);
+		x = new ArrayList<Double>();
+		for (int i = 1; i < numbers.length; ++i) {
+			Double value = Double.parseDouble(numbers[i]);
+			// TODO: Dirty hack for the missing parameter.
+			if (i != 2)
+				x.add(value);
+		}
+
+		int basicFeatureCount = x.size();
+
+		for (int i = 0; i < basicFeatureCount; ++i)
+			for (int j = 0; j < basicFeatureCount; ++j)
+				x.add(x.get(i) * x.get(j));
+
+		for (int i = 0; i < basicFeatureCount; ++i)
+			for (int j = 0; j < basicFeatureCount; ++j)
+				for (int k = 0; k < basicFeatureCount; ++k)
+					x.add(x.get(i) * x.get(j) * x.get(k));
+
+		for (int i = 0; i < basicFeatureCount; ++i)
+			for (int j = 0; j < basicFeatureCount; ++j)
+				for (int k = 0; k < basicFeatureCount; ++k)
+					for (int l = 0; l < basicFeatureCount; ++l)
+						x.add(x.get(i) * x.get(j) * x.get(k) * x.get(l));
+
+	}
 }
--- qa4mre/qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/parameterProcessor/parameterLearner.java
+++ qa4mre/qa4mre-alzheimer-task/src/main/java/edu/cmu/lti/deiis/hw5/parameterProcessor/parameterLearner.java
@@ -8,49 +8,98 @@ import java.util.Random;
 
 public class parameterLearner {
 
-  @SuppressWarnings("resource")
-  static public void main(String[] args) throws Exception {
-    File file = new File("parameterInstance.txt");
-    BufferedReader br = new BufferedReader(new FileReader(file));
-    ArrayList<Instance> instances = new ArrayList<Instance>();
-    String line = br.readLine();
+	@SuppressWarnings("resource")
+	static public void main(String[] args) throws Exception {
+		Double maxScore = 0.;
+		for (int trycount = 0; trycount < 10; ++trycount) {
+			File file = new File("parameterInstance.txt");
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			ArrayList<Instance> instances = new ArrayList<Instance>();
+			String line = br.readLine();
 
-    while (line != null) {
-      instances.add(new Instance(line));
-      line = br.readLine();
-    }
-    ArrayList<Double> result = Train(instances);
-    for(Double d : result){
-      System.out.println(d);
-    }
-  }
+			while (line != null) {
+				instances.add(new Instance(line));
+				line = br.readLine();
+			}
 
-  public static ArrayList<Double> Train(ArrayList<Instance> instances) {
-    ArrayList<Double> result = new ArrayList<Double>(instances.get(0).x);
-    for (int i = 0; i < result.size(); ++i) {
-      result.set(i, 0.);
-    }
-    Random r = new Random();
-    Integer instanceSize = instances.size();
-    for (int epoch = 0; epoch < 10000; ++epoch) {
-      Integer index = r.nextInt() % instanceSize;
-      Double p = (1 + Math.exp(-InnerProduct(result, instances.get(index).x)));
-      Add(result,instances.get(index).x,(instances.get(index).y-p)/epoch);
-    }
-    return result;
-  }
-  
-  public static void Add(ArrayList<Double> v1, ArrayList<Double> v2, Double step){
-    for(int i = 0; i < v1.size(); ++i){
-      v1.set(i, v1.get(i)+v2.get(i)*step);
-    }
-  }
+			ArrayList<Double> result = Train(instances);
+			
+			Double curScore = Test(result, instances);
+			
+			if(curScore > maxScore){
+				maxScore = curScore;
+				for (Double d : result) {
+					System.out.println(d);
+				}
+			}
+		}
+	}
 
-  public static Double InnerProduct(ArrayList<Double> v1, ArrayList<Double> v2) {
-    Double result = 0.;
-    for(int i = 0; i < v1.size(); ++i){
-      result += v1.get(i) * v2.get(i);
-    }
-    return result; 
-  }
+	public static Double Test(ArrayList<Double> w, ArrayList<Instance> instances) {
+		int correct = 0, incorrect = 0;
+		int totalInstance = instances.size();
+		for (int i = 0; i < totalInstance / 5; ++i) {
+			Double trueScore = 0.;
+			Double maxScore = 0.;
+			for (int j = 0; j < 5; ++j) {
+				int index = i * 5 + j;
+				Double p = 1. / (1 + Math.exp(-InnerProduct(w,
+						instances.get(index).x)));
+				if (instances.get(index).y == 1)
+					trueScore = p;
+				if (p > maxScore)
+					maxScore = p;
+			}
+			if (Math.abs(trueScore - maxScore) < 0.0001)
+				++correct;
+			else
+				++incorrect;
+		}
+		System.out.format("correct=%d incorrect=%d\n", correct, incorrect);
+		return correct / new Double(correct + incorrect);
+	}
+
+	public static ArrayList<Double> Train(ArrayList<Instance> instances) {
+		ArrayList<Double> result = new ArrayList<Double>(instances.get(0).x);
+		for (int i = 0; i < result.size(); ++i) {
+			result.set(i, 0.);
+		}
+		Random r = new Random();
+		Integer instanceSize = instances.size();
+		for (int epoch = 1; epoch < 10000000; ++epoch) {
+			Integer index = Math.abs(r.nextInt()) % instanceSize;
+			Double p = 1. / (1 + Math.exp(-InnerProduct(result,
+					instances.get(index).x)));
+			/*
+			 * for (Double d : result) { System.out.format("%f ", d); } for
+			 * (Double d : instances.get(index).x) { System.out.format("%f ",
+			 * d); }
+			 */
+			Double compensation = 1.;
+			if (instances.get(index).y == 1)
+				compensation = 4.00;
+
+			Add(result, instances.get(index).x,
+					compensation * (instances.get(index).y - p) / 100000);
+
+			// System.out.println();
+		}
+		return result;
+	}
+
+	public static void Add(ArrayList<Double> v1, ArrayList<Double> v2,
+			Double step) {
+		// System.out.println(step);
+		for (int i = 0; i < v1.size(); ++i) {
+			v1.set(i, v1.get(i) + v2.get(i) * step);
+		}
+	}
+
+	public static Double InnerProduct(ArrayList<Double> v1, ArrayList<Double> v2) {
+		Double result = 0.;
+		for (int i = 0; i < v1.size(); ++i) {
+			result += v1.get(i) * v2.get(i);
+		}
+		return result;
+	}
 }
